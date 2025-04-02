198ed868584b8000d5a9d52a147b11576f144805
Manon <manon.jacquin@gmail.com>
2025-04-02 08:44:07 -0700

    fix: deployment and adds gcp bucket

--- infra/main.py
+++ infra/main.py
@@ -1,3 +1,18 @@
+import pulumi
+
+from gcp_resources import create_chroma_datastore_bucket
+
+# Get the environment from Pulumi config
+config = pulumi.Config()
+env = config.require("env")  # This will be set in Pulumi.dev.yaml or Pulumi.prod.yaml
+
+# Create the Chroma datastore bucket
+chroma_bucket = create_chroma_datastore_bucket(env)
+
+# Export the bucket name
+pulumi.export("chroma_bucket_name", chroma_bucket.name)
+
+
 def main():
     print("Hello from infra!")
 
